6a83bb30d3f8cf9a6e961a6c57cdbfdd64d9c7a7
RobAdams609 <radams609@gmail.com>
2025-07-21 21:08:10 -0400

    Update metrics.js

--- netlify/functions/metrics.js
+++ netlify/functions/metrics.js
@@ -1,1 +1,6 @@
-// Metrics lambda placeholder
+exports.handler = async function(event, context) {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Metrics function is working." })
+  };
+};
